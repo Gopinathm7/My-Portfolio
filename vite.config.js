a0762eb182f3e0faf0ee660e0dbf3d55a00b8c1b
Gopinath M <gopinathm04@outlook.com>
2025-03-02 15:13:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/My-Portfolio/", // Ensure this matches your repo name
   plugins: [react()],
-  base: 'My-Portfolio', // Replace 'your-repo' with your actual GitHub repository name
 });
+
